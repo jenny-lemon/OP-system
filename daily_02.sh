5fd7f4f604b2077fc69fcdd145a602c372212505
jenny-lemon <jenny@lemonclean.com.tw>
2026-04-11 16:56:21 +0800

    Delete daily_02.sh

--- daily_02.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /Users/jenny/lemon || exit 1
-
-/usr/bin/python3 當月次月訂單.py >> /Users/jenny/lemon/cron.log 2>&1
-sleep 120
-/usr/bin/python3 專員系統個資.py >> /Users/jenny/lemon/cron.log 2>&1
