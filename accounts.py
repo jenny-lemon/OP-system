c10389c7d9c6b15a4b94788fce82b823995d5cd6
jenny-lemon <jenny@lemonclean.com.tw>
2026-04-19 04:31:28 +0800

    Update accounts.py

--- accounts.py
+++ accounts.py
@@ -14,7 +14,7 @@ def _get_secret(path, default=None):
                 value = value[key]
             return value
         except Exception:
-            pass
+            return default
     return default
 
 
@@ -28,27 +28,47 @@ def _pick(secret_path, env_name):
 
 ACCOUNTS = {}
 
+# 台北
 taipei_email = _pick(["accounts", "taipei", "email"], "TAIPEI_EMAIL")
 taipei_password = _pick(["accounts", "taipei", "password"], "TAIPEI_PASSWORD")
 if taipei_email and taipei_password:
-    ACCOUNTS["台北"] = {"email": taipei_email, "password": taipei_password}
+    ACCOUNTS["台北"] = {
+        "email": taipei_email,
+        "password": taipei_password,
+    }
 
+# 台中
 taichung_email = _pick(["accounts", "taichung", "email"], "TAICHUNG_EMAIL")
 taichung_password = _pick(["accounts", "taichung", "password"], "TAICHUNG_PASSWORD")
 if taichung_email and taichung_password:
-    ACCOUNTS["台中"] = {"email": taichung_email, "password": taichung_password}
+    ACCOUNTS["台中"] = {
+        "email": taichung_email,
+        "password": taichung_password,
+    }
 
+# 桃園
 taoyuan_email = _pick(["accounts", "taoyuan", "email"], "TAOYUAN_EMAIL")
 taoyuan_password = _pick(["accounts", "taoyuan", "password"], "TAOYUAN_PASSWORD")
 if taoyuan_email and taoyuan_password:
-    ACCOUNTS["桃園"] = {"email": taoyuan_email, "password": taoyuan_password}
+    ACCOUNTS["桃園"] = {
+        "email": taoyuan_email,
+        "password": taoyuan_password,
+    }
 
+# 新竹
 hsinchu_email = _pick(["accounts", "hsinchu", "email"], "HSINCHU_EMAIL")
 hsinchu_password = _pick(["accounts", "hsinchu", "password"], "HSINCHU_PASSWORD")
 if hsinchu_email and hsinchu_password:
-    ACCOUNTS["新竹"] = {"email": hsinchu_email, "password": hsinchu_password}
+    ACCOUNTS["新竹"] = {
+        "email": hsinchu_email,
+        "password": hsinchu_password,
+    }
 
+# 高雄
 kaohsiung_email = _pick(["accounts", "kaohsiung", "email"], "KAOHSIUNG_EMAIL")
 kaohsiung_password = _pick(["accounts", "kaohsiung", "password"], "KAOHSIUNG_PASSWORD")
 if kaohsiung_email and kaohsiung_password:
-    ACCOUNTS["高雄"] = {"email": kaohsiung_email, "password": kaohsiung_password}
+    ACCOUNTS["高雄"] = {
+        "email": kaohsiung_email,
+        "password": kaohsiung_password,
+    }
